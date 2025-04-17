56b7809f665a9dfa4ad118a25ab363198f1325d4
Kaustubh Ladiya <k4ustu3h@gmail.com>
2025-04-18 03:40:06 +0530

    Add a Skeleton to the CategorySelector

--- src/components/select/CategorySelector.jsx
+++ src/components/select/CategorySelector.jsx
@@ -4,19 +4,23 @@ import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
+import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
 
 export default function CategorySelector({ onCategoryChange, selectedValue }) {
 	const [categories, setCategories] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		axios
 			.get("http://localhost:9090/api/questions/categories")
 			.then((response) => {
 				setCategories(["All", ...response.data]);
+				setLoading(false);
 			})
 			.catch((error) => {
 				console.error("Error fetching categories:", error);
+				setLoading(false);
 			});
 	}, []);
 
@@ -30,22 +34,26 @@ export default function CategorySelector({ onCategoryChange, selectedValue }) {
 			<Typography variant="h6" gutterBottom>
 				Select a category:
 			</Typography>
-			<FormControl fullWidth margin="normal">
-				<InputLabel id="category-select-label">Category</InputLabel>
-				<Select
-					id="category-select"
-					label="Category"
-					labelId="category-select-label"
-					onChange={handleChange}
-					value={selectedValue}
-				>
-					{categories.map((category) => (
-						<MenuItem key={category} value={category}>
-							{category}
-						</MenuItem>
-					))}
-				</Select>
-			</FormControl>
+			{loading ? (
+				<Skeleton width="100%" height={100} />
+			) : (
+				<FormControl fullWidth sx={{ mt: 3 }}>
+					<InputLabel id="category-select-label">Category</InputLabel>
+					<Select
+						id="category-select"
+						label="Category"
+						labelId="category-select-label"
+						onChange={handleChange}
+						value={selectedValue}
+					>
+						{categories.map((category) => (
+							<MenuItem key={category} value={category}>
+								{category}
+							</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+			)}
 		</>
 	);
 }
